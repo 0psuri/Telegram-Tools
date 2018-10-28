02ff4d12af197925eb90fcd7932715fb8a021ca3
3.13.1.5 <35502476+TheDevFromKer@users.noreply.github.com>
2018-10-28 21:33:32 +0100

    Updated

--- genNumbers.py
+++ genNumbers.py
@@ -1,27 +1,34 @@
+# -*- coding: utf-8 -*-
+
 from random import randint
 
-text_file = open("card.vcf", "w")
-text_file2 = open("nums.txt", "w")
-text = ''
-text2 = ''
-
-i = 0
-while i != 500:
-    num = randint(000000000000,000000000000) #range from XXX to XXX
-    
-    text += 'BEGIN:VCARD\n'
-    text += 'VERSION:2.1\n'
-    text += 'N:{}{};{};;;\n'.format("Deanon", i, "Telegram")
-    text += 'FN:{} {}{}\n'.format("Telegram", "Deanon", i)
-    text += 'TEL;CELL:{}\n'.format(num)
-    text += 'END:VCARD\n'
-
-    text2 += '{}\n'.format(num)
+def Generator():
+    for i in range(numbers):
+        num = randint(num_from, num_endp)
         
-    i += 1
-    print(i)
+        text = 'BEGIN:VCARD\n'
+        text += 'VERSION:2.1\n'
+        text += 'N:{}{};{};;;\n'.format("Deanon", i, "Telegram")
+        text += 'FN:{} {}{}\n'.format("Telegram", "Deanon", i)
+        text += 'TEL;CELL:{}\n'.format(num)
+        text += 'END:VCARD\n\n'
 
-text_file.write(text)
-text_file2.write(text2)
-text_file.close()
-text_file2.close()
+        card_file.write(text)
+        text_file.write('{}\n'.format(num))
+            
+        print('Generated: {} of {}'.format(i, numbers) , end='\r')
+
+if __name__ == '__main__':
+    numbers = int(input('Count of number to generate: ') or 500)
+    num_from = int(input('Numbers from (ex. 138000000000): ') or 138000000000)
+    num_endp = int(input('Numbers to (ex. 138999999999): ') or 138999999999)
+
+    print('\n')
+
+    card_file = open("card.vcf", "a")
+    text_file = open("nums.txt", "a")
+
+    Generator()
+
+    card_file.close()
+    text_file.close()
